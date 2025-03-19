d213da329ac961455f124d7d56fee1c28c3201a9
anstjrdud <101007795+anstjrdud@users.noreply.github.com>
2025-03-19 12:20:56 +0900

    Update vision_processing_01.py

--- Vision Processing/vision_processing_01.py
+++ Vision Processing/vision_processing_01.py
@@ -5,11 +5,17 @@ img = cv.imread('mistyroad.jpg')
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
+h = cv.calcHist([gray],[0],None,[256],[0,256])
+
+plt.imshow(gray, cmap='gray'), plt.xticks([]), plt.yticks([]), plt.show()
+
+plt.plot(h, color='r', linewidth=1), plt.show()
+
 t, bin_img = cv.threshold(img[:,:,2], 127, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
 print('오쥬 알고리즘이 찾느 최적 임곗값=', t)
 
-equal = cv.equalizeHist(bin_img)
-plt.imshow(equal, cmap='gray'), plt.xticks([]), plt.yticks([]), plt.show()
+h = cv.calcHist([bin_img],[0],None,[256],[0,256])
 
-h = cv.calcHist([equal],[0],None,[256],[0,256])
+plt.imshow(bin_img, cmap='gray'), plt.xticks([]), plt.yticks([]), plt.show()
+
 plt.plot(h, color='r', linewidth=1), plt.show()
