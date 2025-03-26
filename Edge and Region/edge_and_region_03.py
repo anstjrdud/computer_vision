8dbcf8bd1f59e502e771bbe8c828da570e2ad996
anstjrdud <101007795+anstjrdud@users.noreply.github.com>
2025-03-26 21:21:40 +0900

    Update edge_and_region_03.py

--- Edge and Region/edge_and_region_03.py
+++ Edge and Region/edge_and_region_03.py
@@ -4,57 +4,35 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 # 입력 영상 불러오기
-src = cv.imread('dabotop.jpg')
+src = cv.imread('coffee cup.jpg')
 
 # 사각형 지정을 통한 초기 분할
-mask = np.zeros(src.shape[:2], np.uint8)
-bgdModel = np.zeros((1,65), np.float64)
-fgdModel = np.zeros((1,65), np.float64)
-
-iterCount = 1
-mode = cv.GC_INIT_WITH_RECT
+mask = np.zeros(src.shape[:2], np.uint8) # 마스크
+bgdModel = np.zeros((1, 65), np.float64) # 배경 모델 무조건 1행 65열, float64
+fgdModel = np.zeros((1, 65), np.float64) # 전경 모델 무조건 1행 65열, float64
 
 rc = cv.selectROI(src)
 
-cv.grabCut(src, mask, rc, bgdModel, fgdModel, iterCount, mode)
+# RECT는 사용자가 사각형 지정. 이 값에서 계속 업데이트
+cv.grabCut(src, mask, rc, bgdModel, fgdModel, 1, cv.GC_INIT_WITH_RECT)
 
-mask2 = np.where((mask ==0) | (mask == 2), 0, 1).astype('uint8')
+# mask 4개 값을 2개로 변환
+mask2 = np.where((mask == 0) | (mask == 2), 0, 1).astype('uint8')
 dst = src * mask2[:, :, np.newaxis]
 
-# 초기 분할 결과 출력
-cv.imshow('dst', dst)
+plt.subplot(1, 3, 1)
+plt.imshow(src)
+plt.title('Original')
+plt.axis('off')
 
-def on_mouse(event, x, y, flags, param):
-    if event == cv.EVENT_LBUTTONDOWN:
-        cv.circle(dst, (x,y), 3, (255,0,0), -1)
-        cv.circle(mask, (x,y), 3, cv.GC_FGD, -1)
-        cv.imshow('dst',dst)
-    elif event == cv.EVENT_RBUTTONDOWN:
-        cv.circle(dst, (x,y), 3, (0,0,255), -1)
-        cv.circle(mask, (x,y), 3, cv.GC_BGD, -1)
-        cv.imshow('dst',dst)
-    elif event == cv.EVENT_MOUSEMOVE:
-        if flags & cv.EVENT_FLAG_LBUTTON:
-            cv.circle(dst, (x,y), 3, (255,0,0), -1)
-            cv.circle(mask, (x,y), 3, cv.GC_FGD, -1)
-            cv.imshow('dst',dst)
-        elif flags & cv.EVENT_FLAG_RBUTTON:
-            cv.circle(dst, (x,y), 3, (0,0,255), -1)
-            cv.circle(mask, (x,y), 3, cv.GC_BGD, -1)
-            cv.imshow('dst',dst)
+plt.subplot(1, 3, 2)
+plt.imshow(mask2 * 255)
+plt.title('mask')
+plt.axis('off')
 
-cv.setMouseCallback('dst', on_mouse)
+plt.subplot(1, 3, 3)
+plt.imshow(dst)
+plt.title('Result')
+plt.axis('off')
 
-
-while True:
-    key = cv.waitKey()
-    if key == 13:
-        cv.grabCut(src, mask, rc, bgdModel, fgdModel, 1, cv.GC_INIT_WITH_MASK)
-        mask2 = np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')
-        dst2 = src * mask2[:, :, np.newaxis]
-        cv.imshow('dst',dst)
-    elif key == 27:
-        break
-
-cv.destroyAllWindows()
-
+plt.show()
