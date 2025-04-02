eb7ca6d5e8ca06ecf023cce8c08a606f260c19ee
anstjrdud <101007795+anstjrdud@users.noreply.github.com>
2025-04-02 17:19:21 +0900

    Update local_feature_01.py

--- Local Feature/local_feature_01.py
+++ Local Feature/local_feature_01.py
@@ -2,17 +2,19 @@ import cv2 as cv
 import matplotlib.pyplot as plt
 
 img = cv.imread("mot_color70.jpg")
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
 sift = cv.SIFT_create(nfeatures = 0, nOctaveLayers = 3, contrastThreshold = 0.06, edgeThreshold = 18, sigma = 1.6)
-kp, des = sift.detectAndCompute(gray, None)
+kp, des = sift.detectAndCompute(img, None)
 
-result = cv.drawKeypoints(gray, kp, None, flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+result = cv.drawKeypoints(img, kp, None, flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
 result = cv.cvtColor(result, cv.COLOR_BGR2RGB)
 
+img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+
+
 plt.subplot(1, 2, 1)
-plt.imshow(gray, cmap = 'gray')
+plt.imshow(img)
 plt.title('Original')
 plt.axis('off')
 
